f246221dbe6a9d3f2eb67ddeea35538a3401154c
Aqdas Ghani <aqdasghani2005@gmail.com>
2026-07-21 13:13:15 +0530

    Added herosection

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import younggirl from './assets/younggirl.jpg'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className='bg-yellow-300 rounded-sm w-full h-screen'>
+      <div className='bg-yellow-300 rounded-3xl m-2 w-[98vw] h-[97vh]'>
         <div className='w-full flex justify-center pt-8'>
           <nav className='navbar bg-white rounded-full p-2 shadow-lg'>
             <ul className='flex gap-16 w-full items-center'>
@@ -26,13 +27,24 @@ function App() {
 
 
 
-        <div className=''>
+        <div className='herosection grid grid-cols-2 gap-20 items-center px-10 py-16'>
+          <div className='left-div flex flex-col gap-8'>
+            <h1 className='text-4xl font-bold text-zinc-900 leading-none'>Meet QuickReply !</h1>
+            <p className='text-lg text-zinc-700 leading-7 max-w-xl'>
+              Automate YouTube comment replies with customisable keyword rules. Save time, engage your audience instantly, and never miss repitive questions again.
+
+
+            </p>
+          </div>
+
+
+          <div className='right-div'>
+
+            <img src={younggirl} alt="younggirl" className='w-[500px] rounded-3xl' />
+          </div>
 
 
         </div>
-
-
-
       </div >
 
     </>
